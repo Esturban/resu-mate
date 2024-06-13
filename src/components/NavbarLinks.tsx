9f5e238afcdaf5575c17c19c8437d111fd55b6c3
Esturban <info@evadvisory.ca>
2024-06-13 12:19:50 -0400

    Navbar reference
    Updating the landing page reference behaviour

--- src/components/NavbarLinks.tsx
+++ src/components/NavbarLinks.tsx
@@ -1,8 +1,8 @@
 import { NavbarLinksAbout } from "./NavbarLinksAbout";
-import { NavbarLinksHome } from "./NavbarLinksHome";
 import { NavbarLinksPlatforms } from "./NavbarLinksPlatforms";
 import { NavbarLinksPricing } from "./NavbarLinksPricing";
 import { NavbarLinksSupport } from "./NavbarLinksSupport";
+import { Link } from "react-scroll";
 
 /**
  * create links found in leftside of navbar when expanded
@@ -10,11 +10,19 @@ import { NavbarLinksSupport } from "./NavbarLinksSupport";
 export function NavbarLinks(): JSX.Element {
   return (
     <div className="flex items-center">
-      <h1 className="text-3xl font-bold mr-4 sm:text-4xl cursor-pointer">
-        Lorem ipsum
-      </h1>
+      <Link to="home"
+        smooth
+        duration={500}
+        rel="noopener noreferrer"
+        role="link"
+        title="Home"
+        aria-label="Home">
+        <h1 className="text-3xl font-bold mr-4 sm:text-4xl cursor-pointer">
+          Resumate
+        </h1>
+      </Link>
       <ul className="hidden md:flex cursor-pointer">
-        <li>{NavbarLinksHome()}</li>
+        
         <li>{NavbarLinksAbout()}</li>
         <li>{NavbarLinksSupport()}</li>
         <li>{NavbarLinksPlatforms()}</li>
