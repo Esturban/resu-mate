e59f3a599e5a337ed01ad57fd3dc5784d1760695
Esturban <info@evadvisory.ca>
2024-06-13 15:30:11 -0400

    Hero CTA

--- src/components/Hero/HeroGetStarted.tsx
+++ src/components/Hero/HeroGetStarted.tsx
@@ -1,8 +1,10 @@
 /* get started button in hero element */
+import { Link } from 'react-scroll';
+
 export function HeroGetStarted(): JSX.Element {
   return (
-    <button type="button" className="py-3 sm:w-[70%] pr-4 pl-4 my-4 bg-indigo-700">
-      Dreams Await
-    </button>
+    <Link to="pricing" smooth={true} duration={500} type="button" className="py-3 sm:w-[70%] pr-4 pl-4 my-4 bg-indigo-700 hover:bg-indigo-300 rounded-full transform hover:scale-110 transition-all duration-200 ease-in-out text-white hover:text-white text-center text-lg">
+      Dream Job Awaits
+    </Link>
   );
 }
