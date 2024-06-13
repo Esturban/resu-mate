9418d380be54418e6ab1b0e2853665bdd9fe3f3d
Esturban <info@evadvisory.ca>
2024-06-13 14:58:00 -0400

    Hero
    Moving Hero and updating the elements
    
    Still need to work on the mobile view

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,8 @@
 import { CloudArrowUpIcon, ArrowDownOnSquareStackIcon, PaperAirplaneIcon, ServerIcon } from "@heroicons/react/24/solid";
-import uuid from "react-uuid";
-import { HeroResume } from "./HeroResume";
-import { HeroHeading } from "./HeroHeading";
-import { HeroImage } from "./HeroImage";
+import {v4 as uuid} from "uuid";
+import { HeroResume } from "./Hero/HeroResume";
+import { HeroHeading } from "./Hero/HeroHeading";
+import { HeroImage } from "./Hero/HeroImage";
 
 const data = [
   { component: <CloudArrowUpIcon />, p: "ATS Compatible" },
@@ -12,9 +12,9 @@ const data = [
 ] as const;
 
 export const mappedData = data.map((e) => (
-  <div className="flex px-4 py-2 text-gray-900" key={uuid()}>
-    <div className="h-6 text-indigo-600">{e.component}</div>
-    <p className="text-gray-900">{e.p}</p>
+  <div className="flex flex-col items-center px-4 py-2" key={uuid()}>
+    <div className="w-24 p-4 bg-indigo-600 text-white rounded-lg">{e.component}</div>
+    <p className="text-gray-900 mt-2">{e.p}</p>
   </div>
 ));
 
@@ -25,7 +25,7 @@ export default function Hero(): JSX.Element {
       id="home"
       className="w-full h-screen bg-zinc-200 flex flex-col justify-between"
     >
-      <div className="grid md:grid-cols-2 max-w-[1280px] m-auto">
+      <div className="grid md:grid-cols-2 max-w-[1280px] m-auto px-12">
         <HeroHeading />
         <HeroImage />
           <HeroResume />
--- src/components/Hero/HeroImage.tsx
+++ src/components/Hero/HeroImage.tsx
@@ -1,10 +1,10 @@
 /* creates hero image */
 export function HeroImage(): JSX.Element {
   return (
-    <div>
+    <div className="">
       <img
         className="w-full invisible sm:visible"
-        src="cyber-bg.webp"
+        src="/cyber-bg.webp"
         alt="/"
       />
     </div>
