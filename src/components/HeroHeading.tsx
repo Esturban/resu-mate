2882bb6472c3edc1067ff2294cafdd9abaf93006
Esturban <info@evadvisory.ca>
2024-06-13 12:42:00 -0400

    Updating Hero
    Adding some elements to devour the entire process for CI/CD

--- src/components/HeroHeading.tsx
+++ src/components/HeroHeading.tsx
@@ -4,9 +4,9 @@ import { HeroGetStarted } from "./HeroGetStarted";
 export function HeroHeading(): JSX.Element {
   return (
     <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
-      <p className="text-2xl mt-2 sm:mt-0">Unique Sequencing & Production</p>
-      <h1 className="py-3 text-5xl md:text-7xl font-bold">Cloud Management</h1>
-      <p className="text-2xl">This is our Tech brand.</p>
+      <p className="text-2xl mt-2 sm:mt-0">Resumes that make you shine ✨</p>
+      <h1 className="py-3 text-5xl md:text-7xl font-bold">Resumate</h1>
+      <p className="text-2xl">Get the jobs you deserve</p>
       <HeroGetStarted />
     </div>
   );
