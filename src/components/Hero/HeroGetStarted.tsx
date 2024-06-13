75bdf7cc0b266d503888c6e049ad27a712d1f500
Esturban <info@evadvisory.ca>
2024-06-13 14:58:00 -0400

    Hero
    Moving Hero and updating the elements
    
    Still need to work on the mobile view

--- src/components/Hero/HeroGetStarted.tsx
+++ src/components/Hero/HeroGetStarted.tsx
@@ -1,8 +1,8 @@
 /* get started button in hero element */
 export function HeroGetStarted(): JSX.Element {
   return (
-    <button type="button" className="py-3 px-6 sm:w-[60%] my-4 bg-indigo-700">
-      Get Started
+    <button type="button" className="py-3 sm:w-[70%] pr-4 pl-4 my-4 bg-indigo-700">
+      Dreams Await
     </button>
   );
 }
--- src/components/Hero/HeroHeading.tsx
+++ src/components/Hero/HeroHeading.tsx
@@ -3,7 +3,7 @@ import { HeroGetStarted } from "./HeroGetStarted";
 /* container to hold text and button for hero */
 export function HeroHeading(): JSX.Element {
   return (
-    <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
+    <div className="flex flex-col justify-center items-center md:items-start w-full px-0 md:px-2 py-8">
       <p className="text-2xl mt-2 sm:mt-0">Resumes that make you shine ✨</p>
       <h1 className="py-3 text-5xl md:text-7xl font-bold">Resumate</h1>
       <p className="text-2xl">Get the jobs you deserve</p>
