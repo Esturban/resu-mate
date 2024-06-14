fde0984f9f8f13b1a365fd77d98fff7ccfdd2265
Esturban <info@evadvisory.ca>
2024-06-14 07:26:08 -0400

    Hero Button CTA

--- src/components/Hero/HeroCTA.tsx
+++ src/components/Hero/HeroCTA.tsx
@@ -12,7 +12,7 @@ export function HeroCTA(): JSX.Element {
 
   return (
     <Link onClick={handleClick} to="pricing" smooth={true} duration={500} type="button" className={`py-3 sm:w-[70%] pr-4 pl-4 my-4 bg-indigo-700 hover:bg-indigo-300 rounded-full transform hover:scale-110 transition-all duration-200 ease-in-out hover:text-gray-800 text-white text-center text-lg ${isClicked ? 'animate-bounce' : ''}`}>
-      Dreams Await
+      Dream Job Awaits
     </Link>
   );
 }
