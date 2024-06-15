a4229300ef24f076bf15b1a434027b2f595da928
Esturban <info@evadvisory.ca>
2024-06-15 17:06:18 -0400

    Features Update
    Adding the features elements and editing the copy

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,35 +1,35 @@
 import {v4 as uuid} from "uuid";
-import { ChevronUpIcon, PhoneIcon, UserGroupIcon } from "@heroicons/react/24/solid";
-import { SupportText } from "./Features/FeatureText";
+import { PhoneIcon, UserGroupIcon, HandThumbUpIcon } from "@heroicons/react/24/solid";
+import { FeatureText } from "./Features/FeatureText";
 
 /* support component */
 function Features(): JSX.Element {
   const data = [
     {
       component: <PhoneIcon />,
-      span: "Sales",
-      p: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.Provident quisquam, deserunt molestias neque iusto quos quo? Rerum accusamus facere totam iste doloremque, nisi eum, cupiditate architecto in quam perspiciatis minima.",
+      span: "1. Buy or subscribe",
+      p: "Get a subscription or buy one-time resume update. If you subscribe, you get as many resumes as you'd like with your dream jobs as targets. The more you share, the better we can target your resume",
     },
     {
       component: <UserGroupIcon />,
-      span: "Technical Support",
-      p: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime commodi assumenda architecto adipisci a eaque eum sequi tenetur voluptate doloremque dolor possimus, consectetur molestiae laborum neque sed corporis ad aut!",
+      span: "2. Receive your resume",
+      p: "Receive your resume within 48 hours, on average.",
     },
     {
-      component: <ChevronUpIcon />,
-      span: "Inquiries",
-      p: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius porro laudantium perspiciatis sunt, reprehenderit earum, nihil minima quas aperiam commodi cum assumenda aspernatur hic veritatis id tempora itaque, consequuntur beatae!",
+      component: <HandThumbUpIcon />,
+      span: "3. Get revisions",
+      p: "We'll revise your resume until you're 100% satisfied.",
     },
   ] as const;
 
   const columns = data.map((e) => (
     <div className="bg-white rounded-xl shadow-2xl relative" key={uuid()}>
       <div className="p-8">
-        <div className="w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]">
+        <div className="w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem] mb-4">
           {e.component}
         </div>
         <span className="font-bold text-2xl my-18">{e.span}</span>
-        <p className="text-gray-600 text-xl  mb-12">{e.p}</p>
+        <p className="text-gray-600 text-xl  mb-12 mt-4">{e.p}</p>
       </div>
 
     </div>
@@ -46,7 +46,7 @@ function Features(): JSX.Element {
       </div>
 
       <div className="max-w-[1280px] mx-auto text-white relative">
-        <SupportText />
+        <FeatureText />
         <div className="grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black">
           {columns}
         </div>
