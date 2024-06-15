ce4fbfddf0be7c62994ffe6974322c274a8fc2ea
Esturban <info@evadvisory.ca>
2024-06-15 17:06:18 -0400

    Features Update
    Adding the features elements and editing the copy

--- src/components/Features/FeatureText.tsx
+++ src/components/Features/FeatureText.tsx
@@ -1,18 +1,17 @@
 /* populate element with text */
-export function SupportText(): JSX.Element {
+export function FeatureText(): JSX.Element {
   return (
     <div className="px-4 py-12">
       <h2 className="text-3xl pt-8 text-slate-300 uppercase text-center">
-        Support
+        How It Works
       </h2>
       <h3 className="sm:text-5xl font-bold py-6 text-center capitalize">
-        Finding the right team
+        We make it easy to get your dream job
       </h3>
       <p className="py-4 sm:text-3xl text-slate-300">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-        quaerat quas expedita consequuntur tenetur dolorum dolore aliquid
-        deserunt. Excepturi molestias voluptatem praesentium aut deserunt iusto
-        illo neque tenetur alias voluptate.
+        We designed this service to help people get their foot in the door. Knowing the experience of being 
+        evaluated in a pool of candidates, we know what it takes to stand out. We've helped hundreds of people 
+        in the process get a job they love. We're here to help you too. 
       </p>
     </div>
   );
