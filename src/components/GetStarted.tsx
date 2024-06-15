e103744fc298f1c77e2a875f29a49302f13d214b
Esturban <info@evadvisory.ca>
2024-06-15 17:10:53 -0400

    Pricing Button
    Updating the CTAs and the components of most of the service

--- src/components/GetStarted.tsx
+++ src/components/GetStarted.tsx
@@ -1,8 +1,8 @@
 /* create Get Started button */
 export function GetStarted(): JSX.Element {
   return (
-    <button type="button" className="w-full py-4 -my-1 bottom-0 bg-indigo-700">
-      Get Started
+    <button type="button" className="w-full py-4 -my-1 bottom-0 bg-indigo-700 rounded-full">
+      Your Best Resume
     </button>
   );
 }
