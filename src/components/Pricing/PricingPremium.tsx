dc896dc1bdc0d066ea27a7562c71695850899501
Esturban <info@evadvisory.ca>
2024-06-15 17:10:53 -0400

    Pricing Button
    Updating the CTAs and the components of most of the service

--- src/components/Pricing/PricingPremium.tsx
+++ src/components/Pricing/PricingPremium.tsx
@@ -6,7 +6,7 @@ export function PricingPremium(): JSX.Element {
   return (
     <div className="bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative">
       <span className="uppercase px-3 py-1 bg-indigo-200 text-indigo-900 rounded-2xl text-sm">
-        Premium
+        Subscription
       </span>
       <PricingPremiumMonth />
       <PricingCardRight />
