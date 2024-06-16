ccc63a3f7a63d6a028176d74ed2e536bd5cea706
Esturban <info@evadvisory.ca>
2024-06-16 14:23:22 -0400

    Update About
    Changing elements of the about for more consistent atyling

--- src/components/About/AboutText.tsx
+++ src/components/About/AboutText.tsx
@@ -2,11 +2,16 @@
 export function AboutText(): JSX.Element {
   return (
     <div className="text-center">
-      <h2 className="sm:text-5xl xxs:text-4xl text-2xl font-bold mx-1">
-        Trusted by Legal, Finance, and Healthcare Professionals
+      <h2 className="sm:text-5xl xxs:text-4xl text-3xl font-bold mx-1 mb-3">
+        Trusted by ... 
       </h2>
+      <hr></hr>
+      <h2 className="sm:text-3xl xxs:text-2xl text-xl font-bold mx-1 mt-3">
+      Legal, Finance, and Healthcare Professionals 
+      </h2>
+      
       <p className="sm:text-3xl xxs:text-2xl py-6 text-gray-500 px-5 sm:px-4 md:px-3 lg:px-2 xl:px-1">
-      We use the best resume formulas from <span className="font-bold">ivy league schools and top-tier head hunters.</span>
+      We use the best resume formulas from <span className="font-bold">top-tier head hunters and ivy league schools .</span>
       </p>
       <p className="sm:text-3xl  xxs:text-2xl py-6 text-gray-500 px-5 sm:px-4 md:px-3 lg:px-2 xl:px-1">
       Traditional resumes don't cut it anymore.
