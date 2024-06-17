380ab9b4de49f9a98630bae96f23491869d5d418
Esturban <info@evadvisory.ca>
2024-06-17 13:39:25 -0400

    Abstracting Stripe
    Removing it from direct implementation in the pricing cards
    
    Leaving stripe only for the CTA

--- src/components/Pricing/PricingCardLeft.tsx
+++ src/components/Pricing/PricingCardLeft.tsx
@@ -1,18 +1,12 @@
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
 import  GetStarted  from "../GetStarted";
 import { standard } from "../Pricing";
 
-const stripePromise = loadStripe(process.env.REACT_APP_REACT_APP_PUBLIC_PRODUCT_KEY || '');
 /**
  * create pricing card on the top/left side (based on viewpoint)
  * 
  */
 export function PricingCardLeft(): JSX.Element {
-  const options = {
-    // passing the client secret obtained from the server
-    clientSecret: '{{CLIENT_SECRET}}',
-  };
+  
   return (
     <div className="text-2xl">
       <p className="text-2xl py-4 text-slate-500 text-center mb-0">
@@ -23,9 +17,9 @@ export function PricingCardLeft(): JSX.Element {
       </p>
       <hr></hr>
       {standard}
-      <Elements stripe={stripePromise} options={options}>
+      
       <GetStarted productId={process.env.REACT_APP_STANDARD_PRICE || ''} productMode={'payment'}/>
-      </Elements>
+      
     </div>
   );
 }
