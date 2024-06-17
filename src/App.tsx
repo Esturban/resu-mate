69308733479946bfd595d3b48acca98c3abb784f
Esturban <info@evadvisory.ca>
2024-06-17 08:52:56 -0400

    Editing template
    Updating the title mostly

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// import * as dotenv from "dotenv";
 import About from "./components/About";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
@@ -6,8 +7,13 @@ import Process from "./components/Process";
 import Pricing from "./components/Pricing";
 import Features from "./components/Features";
 
+// dotenv.config();
+
 /* main app entry */
 export default function App() {
+  // console.log(process.env.REACT_APP_YOUR_API_KEY);
+  // console.log(process.env.REACT_APP_STRIPE_URL);
+  // console.log(process.env.REACT_APP_STRIPE_API_KEY);
   return (
     <>
       <Navbar />
