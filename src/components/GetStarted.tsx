1043af56dc4fa55f7fb41f09cd0a033594cf5bce
Esturban <info@evadvisory.ca>
2024-06-17 11:10:28 -0400

    Update CTA
    Updating the CTA button to be sure it's at least a little nicer in each view

--- src/components/GetStarted.tsx
+++ src/components/GetStarted.tsx
@@ -1,7 +1,7 @@
 /* create Get Started button */
 export function GetStarted(): JSX.Element {
   return (
-    <button type="button" className="w-full py-4 -my-1 bottom-0 bg-indigo-700 rounded-full">
+    <button type="button" className="w-full py-4 -my-1 bottom-0 bg-indigo-700 rounded-full mt-2">
       Your Best Resume
     </button>
   );
