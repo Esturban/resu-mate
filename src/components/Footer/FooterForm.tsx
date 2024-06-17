b94565b61b65544cc3db0c173fbb56cb603a9492
Esturban <info@evadvisory.ca>
2024-06-17 11:49:57 -0400

    Update Footer CSS + Attributes
    Editing the attributes to be able to be beautiful in a responsive design

--- src/components/Footer/FooterForm.tsx
+++ src/components/Footer/FooterForm.tsx
@@ -3,7 +3,7 @@ export function FooterForm(): JSX.Element {
   return (
     <form className="flex flex-col sm:flex-row justify-center">
       <input
-        className="w-1/2 xxs:w-full xs:w-full p-2 mr-4 rounded-md mb-4 bg-white text-gray-900"
+        className="xl:w-1/2 lg:w-1/2 xxs:w-full xs:w-full p-2 mr-4 rounded-md mb-4 bg-white text-gray-900"
         type="email"
         placeholder="Your email.."
       />
