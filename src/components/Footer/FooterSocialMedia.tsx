9a3a897706dbe91c376a0567a62a51b70b672938
Esturban <info@evadvisory.ca>
2024-06-17 11:30:37 -0400

    Footer elements
    Removing the necessary stuff, updating the styling

--- src/components/Footer/FooterSocialMedia.tsx
+++ src/components/Footer/FooterSocialMedia.tsx
@@ -1,20 +1,17 @@
 import {
-  FaFacebook,
-  FaGithub,
-  FaInstagram,
   FaTwitter,
-  FaTwitch,
+  FaLinkedin
 } from "react-icons/fa";
 
 /* create social media buttons in footer */
 export function FooterSocialMedia(): JSX.Element {
   return (
     <div className="flex justify-between sm:w-[300px] pt-4 text-2xl cursor-pointer">
-      <FaFacebook />
-      <FaInstagram />
-      <FaTwitter />
-      <FaTwitch />
-      <FaGithub />
+      <a href="https://twitter.com/your_twitter_handle" target="_blank" rel="noopener noreferrer">
+      <FaTwitter color="white"/>
+      </a>
+      <a href="https://twitter.com/your_twitter_handle" target="_blank" rel="noopener noreferrer">
+      <FaLinkedin color="white" /></a>
     </div>
   );
 }
