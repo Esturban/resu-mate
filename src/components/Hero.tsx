29f11d4b486fed9c136801506ab9361dbf491601
Esturban <info@evadvisory.ca>
2024-06-20 17:44:36 -0400

    Adding Logos
    Updating the main page to include the logos, etc.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import {v4 as uuid} from "uuid";
 // import { HeroResume } from "./Hero/HeroResume";
 import { HeroHeading } from "./Hero/HeroHeading";
 import { HeroImage } from "./Hero/HeroImage";
-
+import HeroLogos  from "./Hero/HeroLogos";
 const data = [
   { component: <CloudArrowUpIcon />, p: "ATS Compatible" },
   { component: <ArrowDownOnSquareStackIcon />, p: "Achievement-based" },
@@ -13,7 +13,7 @@ const data = [
 
 export const mappedData = data.map((e) => (
   <div className="flex flex-col items-center px-4 py-2" key={uuid()}>
-    <div className="w-24 p-4 bg-indigo-600 text-white rounded-lg">{e.component}</div>
+    <div className="w-24 p-4 bg-sky-600 text-white rounded-lg">{e.component}</div>
     <p className="text-gray-900 mt-2">{e.p}</p>
   </div>
 ));
@@ -23,16 +23,19 @@ export default function Hero(): JSX.Element {
   return (
     <div
       id="home"
-      className="relative h-screen overflow-hidden bg-indigo-100"
+      className="relative h-screen overflow-hidden bg-sky-100"
     >
       <HeroImage />
       <div className="absolute inset-0 bg-white opacity-25"></div>
       {/* <header className="absolute top-0 left-0 right-0 z-20"> */}
         {/* Your Navbar goes here */}
       {/* </header> */}
-      <div className="container relative z-10 flex items-center py-32 mx-2 md:px-12 xl:py-40">
+      <div className="container relative z-10 flex items-center py-32 mx-2 md:px-12 xl:py-40 xxl:mb-50 xl:mb-80 xl:py-auto">
         <HeroHeading />
+        
       </div>
+      
+      <HeroLogos />
     </div>
   );
 }
