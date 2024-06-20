22ce4e3178be6c2549fbf17aed9b0cb4b7ad5484
Esturban <info@evadvisory.ca>
2024-06-20 17:44:06 -0400

    Hero Update
    Changing of colours and adding the Logos elements

--- src/components/Hero/HeroCTA.tsx
+++ src/components/Hero/HeroCTA.tsx
@@ -11,7 +11,7 @@ export function HeroCTA(): JSX.Element {
   };
 
   return (
-    <Link onClick={handleClick} to="pricing" smooth={true} duration={500} type="button" className={`py-3 sm:w-[70%] pr-4 pl-4 my-4 bg-indigo-700 hover:bg-indigo-300 rounded-full transform hover:scale-110 transition-all duration-200 ease-in-out hover:text-gray-800 text-white text-center text-lg ${isClicked ? 'animate-bounce' : ''}`}>
+    <Link onClick={handleClick} to="pricing" smooth={true} duration={500} type="button" className={`py-3 sm:w-[70%] pr-4 pl-4 my-4 bg-sky-700 hover:bg-sky-300 rounded-full transform hover:scale-110 transition-all duration-200 ease-in-out hover:text-gray-800 text-white text-center text-lg ${isClicked ? 'animate-bounce' : ''}`}>
       Dream Job Awaits
     </Link>
   );
---
+++ src/components/Hero/HeroLogos.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import Marquee from 'react-fast-marquee';
+
+export default function HeroLogos(): JSX.Element {
+  const [images, setImages] = useState<{ src: string; name: string }[]>([]);
+
+  useEffect(() => {
+    const loadImages = async () => {
+      const contexts = require.context('../../assets/logos', false, /\.svg$/);
+      const importPromises = contexts.keys().map(async (key) => {
+        const module = await import(`../../assets/logos/${key.replace('./', '')}`);
+        return {
+          src: module.default,
+          name: key.replace('./', '').replace('.svg', '')
+        };
+      });
+
+      const importedImages = await Promise.all(importPromises);
+      setImages(importedImages);
+    };
+
+    loadImages();
+  }, []);
+
+  return (
+    <div className='relative'>
+      <h2 className="absolute top-[-100px] left-1/2 transform -translate-x-1/2 sm:text-5xl xxs:text-4xl text-3xl font-bold mx-1 text-center">Companies our clients work at:</h2>
+      <hr></hr>
+      <div className='bottom-0 left-0 right-0 mx-auto flex justify-center pb-4 sm:pb-8 md:pb-12 lg:pb-16 w-3/4'>   
+        <Marquee className="lg:rounded-full bg-gray-100 py-2 opacity-70 w-full bg-slate-200" gradient={false} loop={0} speed={100}>
+          {images.map((image, index) => (
+            !image.name.includes('exclude-pattern') && (
+              <img className='p-2 mx-10 ' // Use Tailwind CSS for margin and padding
+                key={index}
+                src={image.src}
+                alt={`${image.name} Logo`}
+                style={{ width: '100px' }} // Removed inline margin
+                loading='lazy'
+              />
+            )
+          ))}
+        </Marquee>
+      </div>
+    </div>
+  );
+}
