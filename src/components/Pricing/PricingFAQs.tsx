e907fe208cbbb40f0cec412bf0eac53fe5c0a581
Esturban <info@evadvisory.ca>
2024-06-21 09:30:51 -0400

    Pricing FAQs

--- src/components/Pricing/PricingFAQs.tsx
+++ src/components/Pricing/PricingFAQs.tsx
@@ -1,71 +1,37 @@
 import { Accordion } from "flowbite-react";
-
+import { CheckMark } from "../CheckMark";
 export function Component() {
   return (
-    <Accordion>
+    <Accordion collapseAll>
       <Accordion.Panel>
-        <Accordion.Title>What can I expect from my purchase?</Accordion.Title>
+        <Accordion.Title className="text-2xl">What can I expect from my purchase?</Accordion.Title>
         <Accordion.Content>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-            Resumate is a service handled via e-mail that will provide you with a form that you fill out including
-          </p>
-          <p className="text-gray-500 dark:text-gray-400">
-            Check out this guide to learn how to&nbsp;
-            <a
-              href="https://flowbite.com/docs/getting-started/introduction/"
-              className="text-cyan-600 hover:underline dark:text-cyan-500"
-            >
-              get started&nbsp;
-            </a>
-            and start developing websites even faster with components on top of Tailwind CSS.
+          <p className="mb-2 text-gray-500 dark:text-gray-400 text-lg">
+            Resumate is a white glove service for all your resume needs. You'll complete a form that takes 5 minutes which requires:
+            <ul>
+            <li><CheckMark text={"Current Resume"} /></li>
+            <li><CheckMark text={"URL of dream jobs"} /></li>
+            <li><CheckMark text={"Your LinkedIn Profile"}/></li>
+            </ul>
+            We take it from there to get your resume in 48 hours.
           </p>
         </Accordion.Content>
       </Accordion.Panel>
       <Accordion.Panel>
-        <Accordion.Title>Is there a Figma file available?</Accordion.Title>
+        <Accordion.Title className="text-2xl">How long does it take?</Accordion.Title>
         <Accordion.Content>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-            Flowbite is first conceptualized and designed using the Figma software so everything you see in the library
-            has a design equivalent in our Figma file.
+          <p className="mb-2 text-gray-500 dark:text-gray-400 text-lg">
+            We promise to deliver your resume within 48 hours for one-off resumes! For subscriptions, we deliver within 24 hours for those actively seeking, plus a bunch of other perks!
           </p>
-          <p className="text-gray-500 dark:text-gray-400">
-            Check out the
-            <a href="https://flowbite.com/figma/" className="text-cyan-600 hover:underline dark:text-cyan-500">
-              Figma design system
-            </a>
-            based on the utility classes from Tailwind CSS and components from Flowbite.
-          </p>
+          
         </Accordion.Content>
       </Accordion.Panel>
       <Accordion.Panel>
-        <Accordion.Title>What are the differences between Flowbite and Tailwind UI?</Accordion.Title>
+        <Accordion.Title className="text-2xl">What if I don’t have a resume?</Accordion.Title>
         <Accordion.Content>
           <p className="mb-2 text-gray-500 dark:text-gray-400">
-            The main difference is that the core components from Flowbite are open source under the MIT license, whereas
-            Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone
-            components, whereas Tailwind UI offers sections of pages.
+          No problem! We’ll gather your info and create a stellar resume from scratch. You don't need an old resume to get started.
           </p>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-            However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no
-            technical reason stopping you from using the best of two worlds.
-          </p>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
-          <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
-            <li>
-              <a href="https://flowbite.com/pro/" className="text-cyan-600 hover:underline dark:text-cyan-500">
-                Flowbite Pro
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://tailwindui.com/"
-                rel="nofollow"
-                className="text-cyan-600 hover:underline dark:text-cyan-500"
-              >
-                Tailwind UI
-              </a>
-            </li>
-          </ul>
         </Accordion.Content>
       </Accordion.Panel>
     </Accordion>
