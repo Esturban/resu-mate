763b9d9e11c877efa034b8f000d17a865cf20f31
Esturban <info@evadvisory.ca>
2024-06-27 14:49:01 -0400

    Hero Update
    Cleaning up the defaults on the hero's CSS and styling, mostly for mobile

--- src/components/Hero/HeroLogos.tsx
+++ src/components/Hero/HeroLogos.tsx
@@ -24,8 +24,8 @@ export default function HeroLogos(): JSX.Element {
 
   return (
     <div className='relative xl:h-3/4'>
-      <h2 className="absolute top-[-100px] left-1/2 transform -translate-x-1/2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mx-1 text-center py-10">Companies our clients work at:</h2>
-      <div className='bottom-0 left-0 right-0 mx-auto flex justify-center pb-4 sm:pb-8 md:pb-12 lg:pb-16 xl:w-3/4 md:w-full'>   
+      <h2 className="absolute top-[-100px] left-1/2 transform -translate-x-1/2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mx-1 text-center mt-4 py-10">Companies our clients work at:</h2>
+      <div className='bottom-0 top-[-100px] left-0 right-0 mx-auto flex justify-center pb-4 sm:pb-8 md:pb-12 lg:pb-16 xl:w-3/4 md:w-full'>   
         <Marquee className="rounded-lg bg-gray-100 py-2 opacity-70 w-full bg-slate-200 mt-10" gradient={false} loop={0} speed={100}>
           {images.map((image, index) => (
             !image.name.includes('exclude-pattern') && (
